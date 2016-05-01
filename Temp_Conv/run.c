2bfa361d1fca982ce40936dbb8c6f8e032542322
Chris <christopher.santos@maine.edu>
2016-05-01 12:30:52 -0400

    Shows both celsius and farenheight

--- Temp_Conv/run.c
+++ Temp_Conv/run.c
@@ -1,10 +1,11 @@
- #include "stm32l4xx_hal.h"
- #include "stm32l476g_discovery.h"
- #include "ece486.h"
- #include <stdlib.h>
- #include <stdio.h>
- #include <math.h>
+#include "stm32l4xx_hal.h"
+#include "stm32l476g_discovery.h"
+#include "ece486.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
 
+#define round(x) ((int)(x)+0.5)
 
 extern FlagStatus KeyPressed;   // Use to detect button presses
 
@@ -21,13 +22,14 @@ int main(void)
 
     int flag = 1;
     int i;
-    float T;
+    float T,V,C,F;
+    int a,b;
     float *input = (float *)malloc(sizeof(float)*1);
     float *stage1_input_re = (float *)malloc(sizeof(float)*1);
 
     setblocksize(1);
 
-    initialize(FS_48K, STEREO_IN, STEREO_OUT);       // Set up: ADC input, DAC output
+    initialize(FS_2K, STEREO_IN, STEREO_OUT);       // Set up: ADC input, DAC output
 
 
     while(1){
@@ -54,12 +56,14 @@ int main(void)
             BSP_LED_Toggle(LED5);
         }
 
-        T = 1.5 + (input[0]*1.5);
-        T = (159.2826*T) - 273.15;
-        T = T*1.8 +32;
-        for(i = 0; i < 20000000; i++){}
+        V = 1.5 + (input[0]*1.5);
+        C = (159.3448842*V) - 273.15;
+        F = C*1.8 + 32;
+        a = (int)(C + 0.5);
+        b = (int)(F + 0.5);
+        for(i = 0; i < 40000000; i++){}
         BSP_LCD_GLASS_DisplayString( (uint8_t *)lcd_str);
-        sprintf(lcd_str, "%2f", T);
+        sprintf(lcd_str, "C%2dF%2d", a, b);
     }
     return 0;
 }
