40b79f1bf83b48b4243809a14517712aec82d2f4
Chris <christopher.santos@maine.edu>
2016-05-02 16:10:24 -0400

    Cleaned up and commented

--- Temp_Conv/run.c
+++ Temp_Conv/run.c
@@ -7,6 +7,7 @@
 
 #define round(x) ((int)(x)+0.5)
 #define N 1
+#define eta 0.987
 
 extern FlagStatus KeyPressed;   // Use to detect button presses
 
@@ -15,7 +16,6 @@ int main(void)
     char lcd_str[8];
     int flag = 1;
     int i,iF,iC;
-    float eta = 0.994;
     float V,C,F,VRef[1];
     VRef[0] = -0.45466666666;
     float *input = (float *)malloc(sizeof(float)*N);
@@ -25,8 +25,6 @@ int main(void)
     initialize(FS_2K, MONO_IN, MONO_OUT);       // Set up: ADC input, DAC output
 
     while(1){
-        //DIGITAL_IO_SET(); 	// Use a scope on PD0 to measure execution time
-
         getblock(input);	// Wait here until the input buffer is filled...
 
         if (KeyPressed) {
@@ -34,15 +32,25 @@ int main(void)
             flag = !flag;
             BSP_LED_Toggle(LED5);
         }
-
+        // Calculate voltage value from normalized input
         V = 1.5 + (input[0]*1.5);
+
+        // Calculate temperature in both F and C
         C = (159.2826542*V*eta) - 273.15;
         F = C*1.8 + 32;
+
+        // Cast to int
         iC = (int)(C + 0.5);
         iF = (int)(F + 0.5);
+
+        // Delay to display not so quickly
         for(i = 0; i < 20000000; i++){}
+
+        // Display to LCD
         BSP_LCD_GLASS_DisplayString((uint8_t *)lcd_str);
         sprintf(lcd_str, "C%2dF%2d", iC, iF);
+
+        // Output reference to the circuit
         putblock(VRef);
     }
     return 0;
